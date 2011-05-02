6f1f1964e44b4ea720b573f689ca7f39163d03bd
Danigb <danigb@gmail.com>
2011-05-02 15:35:56 +0200

    rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joexpo::Application.config.secret_token = '0cf5f35fece23fdf174da0b73e42312d4720f45ef8d2ab0c56a778194fe25bac5388b7252c58ccb704d727ff76543354a0c72e0d1da7d4223c4eef900cc98afc'
